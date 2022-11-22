f5082ecfb235c5ac511000ea1c7bb55f184a50a0
Mohit3312 <mohit3312@gmail.com>
2022-11-23 01:09:14 +0530

    Created My Todos List app version no. 1.1

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque nobis quis, nesciunt, veniam ducimus delectus placeat provident distinctio accusantium at, tempora praesentium ut hic sapiente nulla. Laboriosam quos, veniam enim amet sequi eos eligendi?</p>
+    </div>
+  )
+}
+
+export default About
